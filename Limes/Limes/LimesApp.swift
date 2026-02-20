e2155e76fbdc7aa6d0526b1a151cf3eae7e5622d
Ballenb <ballenb@telkom.co.za>
2026-02-20 21:53:32 +0200

    Project Structure (initial commit)  - AuthView.swift (Login & Register)  - AuthViewModel.swift  - WelcomeViewModel.swift   - RegistrationDoneView.swift  - HomeTabView.swift   - HomeView.swift   - PaymentView.swift   - SettingsView.swift   - TransfarView.swift

---
+++ Limes/Limes/LimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimesApp.swift
+//  Limes
+//
+//  Created by Idol MacBook Pro on 2026/02/20.
+//
+
+import SwiftUI
+
+@main
+struct LimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Limes/LimesUITests/LimesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LimesUITestsLaunchTests.swift
+//  LimesUITests
+//
+//  Created by Idol MacBook Pro on 2026/02/20.
+//
+
+import XCTest
+
+final class LimesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
